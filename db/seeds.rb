f9b255824d6b2706f25dc8c87e8c6dde26c2b7ff
Bruncky <brunobrunckhorst@gmail.com>
2020-02-18 21:50:34 +0100

    Changed seed to have longer texts to work with\nAdded more SCSS to homepage and Articles list

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times do
-  Article.new(title: Faker::Games::Zelda.character, content: Faker::Games::Zelda.item).save
+  Article.new(title: Faker::Games::Zelda.character, content: Faker::Lorem.paragraph).save
 end
